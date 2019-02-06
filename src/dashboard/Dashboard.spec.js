12de114ce5dd6d0b1dafb1ca0ccb1a7d2f9999f7
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-05 21:08:01 -0600

    tests for Dashboard and Display components are written and are passing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+describe('the dashboard component', () => {
+  test('checks if gate defaults to display "unlocked" and "open"', () => {
+    const { getByText } = render(<Dashboard />);
+    getByText('Unlocked');
+    getByText('Open');
+  });
+  test('confirms that the display and controls are rendered', () => {
+    const { getByTestId } = render(<Dashboard />);
+    getByTestId('lockBtn');
+    getByTestId('openBtn');
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+describe('the display component', () => {
+  test('checks for red-led class when door is locked or closed', () => {
+    const { getByText } = render(<Display closed="closed" locked="locked" />);
+
+    expect(getByText(/closed/i)).toHaveClass('red-led');
+    expect(getByText(/locked/i)).toHaveClass('red-led');
+  });
+  test('checks for green-led class when door is locked or closed', () => {
+    const { getByText } = render(<Display closed="" locked="" />);
+
+    expect(getByText(/open/i)).toHaveClass('green-led');
+    expect(getByText(/unlocked/i)).toHaveClass('green-led');
+  });
+});
