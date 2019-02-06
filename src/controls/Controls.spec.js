97cf2f7663d449bbcb6e63e55d707bf2e7e9ae11
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-05 21:53:54 -0600

    wrote more tests on the Dashboard component and wrote the Controls component tests.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,22 @@
-// Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './Controls';
+
+describe('the controls component', () => {
+  test('checks if there are buttons to toggle between open/closed and locked/unlocked', () => {
+    const { getByText, getByTestId } = render(<Controls />);
+    getByTestId('openBtn');
+    getByTestId('lockBtn');
+  });
+  test('checks if locked toggle button is disabled if the gate is open', () => {
+    // gate is open on initial load
+    const { getByText } = render(<Controls />);
+    expect(getByText(/lock gate/i)).toBeDisabled();
+  });
+  test('checks if open/close toggle button is disabled if the gate is locked', () => {
+    // closing and locking gate with the passed in props. Should result in disabled 'open' button
+    const { getByText } = render(<Controls closed="closed" locked="locked" />);
+    expect(getByText(/open gate/i)).toBeDisabled();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 // Test away
 import React from 'react';
-import { render, fireEvent, cleanup } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import Dashboard from './Dashboard';
 
@@ -14,5 +14,19 @@ describe('the dashboard component', () => {
     const { getByTestId } = render(<Dashboard />);
     getByTestId('lockBtn');
     getByTestId('openBtn');
+  });
+  test('checks if the buttons text toggles between open/close and lock/unlock', () => {
+    const { getByText, getByTestId } = render(<Dashboard />);
+    const openBtn = getByTestId('openBtn');
+    const lockBtn = getByTestId('lockBtn');
+
+    expect(openBtn).toHaveTextContent(/close gate/i);
+    expect(lockBtn).toHaveTextContent(/lock gate/i);
+
+    fireEvent.click(openBtn);
+    fireEvent.click(lockBtn);
+
+    expect(openBtn).toHaveTextContent(/open gate/i);
+    expect(lockBtn).toHaveTextContent(/unlock gate/i);
   });
 });
